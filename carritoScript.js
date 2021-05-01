63d194476e47f4903c343ed6610a05c2bb891ba4
Joahan Sebatian Silva <62309125+S3B4S10@users.noreply.github.com>
2021-05-01 12:59:49 -0500

    origen

--- carritoScript.js
+++ carritoScript.js
@@ -7,9 +7,12 @@ window.onload = function main() {
 function all () {
  const botones = document.querySelectorAll('.card a');
  let carrito = {};
- const items = document.getElementById('items')
- const totales = document.getElementById('totales')
- const fragmentTotales = document.createDocumentFragment()
+ const items = document.getElementById('items');
+ const totales = document.getElementById('totales');
+ const fragmentTotales = document.createDocumentFragment();
+ const templateTotales = document.getElementById('template-totales').content;
+ const templateCarrito = document.getElementById('template-carrito').content;
+ const fragmentCarrito = document.createDocumentFragment();
 
 
     botones.forEach(btn =>{
@@ -20,14 +23,17 @@ function all () {
                 producto.name=btn.parentElement.children.item(0).innerHTML;
                 producto.price=btn.parentElement.children.item(1).innerHTML;
                 producto.id=btn.parentElement.parentElement.id;
+                producto.iva=(producto.price/1.19).toFixed(0);
                 producto.cantidad=1;
+
+                console.log(producto.iva);
                if(carrito.hasOwnProperty(producto.id)){
                    
                 producto.cantidad=carrito[producto.id].cantidad + 1;
                }
             
                carrito[producto.id]={...producto}
-               //console.log(carrito);
+              
                llenarCarrito();
            
             
@@ -36,51 +42,48 @@ function all () {
     const llenarCarrito = () => {
 
         items.innerHTML = '';
-        const template = document.getElementById('template-carrito').content;
-        const fragment = document.createDocumentFragment();
+       
 
         Object.values(carrito).forEach(producto => {
           
           
  
           
-          template.getElementById('idProducto').textContent = producto.id;
-          template.getElementById('name').textContent = producto.name;
-          template.getElementById('cantidad').textContent = producto.cantidad;
-          template.querySelector('span').textContent = producto.price * producto.cantidad;
-    
-          
-           //botones
-           template.getElementById('btn+').dataset.id = producto.id;
-           template.getElementById('btn-').dataset.id = producto.id ;
+          templateCarrito.getElementById('idProducto').textContent = producto.id;
+          templateCarrito.getElementById('name').textContent = producto.name;
+          templateCarrito.getElementById('cantidad').textContent = producto.cantidad;
+          templateCarrito.getElementById('precio').textContent = producto.price * producto.cantidad;
+          templateCarrito.getElementById('iva').textContent = producto.iva * producto.cantidad;
+
+           templateCarrito.getElementById('btn+').dataset.id = producto.id;
+           templateCarrito.getElementById('btn-').dataset.id = producto.id ;
        
 
-        const clone = template.cloneNode(true)
-        fragment.appendChild(clone)
+        const clone = templateCarrito.cloneNode(true)
+        fragmentCarrito.appendChild(clone)
         })
         
-    items.appendChild(fragment)
+    items.appendChild(fragmentCarrito)
     totales.innerHTML = ''
 
     if (Object.keys(carrito).length === 0) {
-        totales.innerHTML = `
-        <th scope="row" colspan="5">Carrito vacío con innerHTML</th>
-        `
+        totales.innerHTML = ""
         return
     }
 
-    const template1 = document.querySelector('#template-footer').content
+    
     
 
     // sumar cantidad y sumar totales
-    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
-    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0)
-    // console.log(nPrecio)
+    const cantidadTotal = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0)
+    const precioTotal = Object.values(carrito).reduce((acc, {cantidad, price}) => acc + cantidad * price ,0)
+    const ivaTotal=  Object.values(carrito).reduce((acc, {cantidad, iva}) => acc + cantidad * iva ,0)
 
-    template1.querySelectorAll('td')[0].textContent = nCantidad
-    template1.querySelector('span').textContent = nPrecio
+    templateTotales.getElementById('cantidadTotal').textContent = cantidadTotal
+    templateTotales.getElementById('valorTotal').textContent = precioTotal;
+    templateTotales.getElementById('ivaTotal').textContent = ivaTotal;
 
-    const clone = template1.cloneNode(true)
+    const clone = templateTotales.cloneNode(true)
     fragmentTotales.appendChild(clone)
 
     totales.appendChild(fragmentTotales)
@@ -88,31 +91,30 @@ function all () {
 
     const boton = document.querySelector('#vaciar-carrito')
     boton.addEventListener('click', () => {
-        carrito = {}
-        totales.innerHTML = `
-        <th scope="row" colspan="5">Carrito vacío con innerHTML</th>
-        `
-       items.innerHTML = '';
+       /* carrito = {};
+        totales.innerHTML ='';
+       items.innerHTML = '';*/
+
+       
     })
       
-    //controlCantidad()
+    controlCantidad()
 
     }
 
 
-const accionBotones = () => {
+const controlCantidad = () => {
     const botonesAgregar = document.querySelectorAll('#items .btn-info')
     const botonesEliminar = document.querySelectorAll('#items .btn-danger')
 
-    // console.log(botonesAgregar)
-
+    
     botonesAgregar.forEach(btn => {
         btn.addEventListener('click', () => {
-            // console.log(btn.dataset.id)
+            
             const producto = carrito[btn.dataset.id]
             producto.cantidad ++
             carrito[btn.dataset.id] = { ...producto }
-            pintarCarrito()
+           llenarCarrito()
         })
     })
 
@@ -126,7 +128,7 @@ const accionBotones = () => {
             } else {
                 carrito[btn.dataset.id] = { ...producto }
             }
-            pintarCarrito()
+            llenarCarrito()
         })
     })
 }
